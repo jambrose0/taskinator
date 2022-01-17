e57d451e6755a608e21dc234f0a40fbfb96ad403
jambrose0 <jambrose0518@gmail.com>
2022-01-17 17:34:32 -0500

    seperated the two functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,17 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    console.dir(taskTypeInput);
   //create list item
    var listItemEl = document.createElement("li");
      listItemEl.className = "task-item";
-   listItemEl.textContent = "This is a new task.";
+     var taskInfoEl = document.createElement("div");
+     taskInfoEl.className = "task-info";
+     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+     listItemEl.appendChild(taskInfoEl);
+   listItemEl.textContent = taskNameInput;
    tasksToDoEl.appendChild(listItemEl);
 
    
@@ -22,12 +29,12 @@ var createTaskHandler = function(event) {
 formEl.addEventListener("submit", createTaskHandler);
 
 
-// var taskNameInput = document.querySelector("input[name='task-name']").value;
-// var taskTypeInput = document.querySelector("select[name='task-type']").value;
+// 
+// 
 // create div  to hold task info and add to list item
-// var taskInfoEl = document.createElement("div");
-// taskInfoEl.className = "task-info";
+// 
+// 
 //add html content to div
-// taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
-// listItemEl.appendChild(taskInfoEl);
+// 
+// 
 //add entire list item to list      
